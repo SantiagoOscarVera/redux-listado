e27d591258f3907d4129db6999577f96fe18a3e9
Puchoanimal <santiagovera280@gmail.com>
2023-04-21 00:16:03 -0300

    listo

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,57 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react'; // el useEffect conecta a los componentes de la aplicacion
+import { fetchAllUsers } from '../store/reducer-slices/users';
+import { useDispatch, useSelector } from 'react-redux';
 
 const UserList = () => {
 
-    const users = [{
-        "id": 1,
-        "email": "george.bluth@reqres.in",
-        "first_name": "George",
-        "last_name": "Bluth",
-        "avatar": "https://reqres.in/img/faces/1-image.jpg"
-    },
-    {
-        "id": 2,
-        "email": "janet.weaver@reqres.in",
-        "first_name": "Janet",
-        "last_name": "Weaver",
-        "avatar": "https://reqres.in/img/faces/2-image.jpg"
-    },
-    {
-        "id": 3,
-        "email": "emma.wong@reqres.in",
-        "first_name": "Emma",
-        "last_name": "Wong",
-        "avatar": "https://reqres.in/img/faces/3-image.jpg"
-    },
-    {
-        "id": 4,
-        "email": "eve.holt@reqres.in",
-        "first_name": "Eve",
-        "last_name": "Holt",
-        "avatar": "https://reqres.in/img/faces/4-image.jpg"
-    },
-    {
-        "id": 5,
-        "email": "charles.morris@reqres.in",
-        "first_name": "Charles",
-        "last_name": "Morris",
-        "avatar": "https://reqres.in/img/faces/5-image.jpg"
-    },
-    {
-        "id": 6,
-        "email": "tracey.ramos@reqres.in",
-        "first_name": "Tracey",
-        "last_name": "Ramos",
-        "avatar": "https://reqres.in/img/faces/6-image.jpg"
-    },
-    ]
+    const {list: users } = useSelector(state => state.users); // users que viene de los slides
+
+    const dispatch = useDispatch();
+
+    useEffect (() => {
+        dispatch(fetchAllUsers());
+    }, [dispatch])
 
     return (
-        <div className='container'>
+        <div className='container mt-4'>
             <div className='row'>
                 {
                     users.map((user, index) => (
-                        <div key={index} className='col-md-3'>
+                        <div key={index} className='col-md-3 mb 4'>
                             <div className='card'>
                                 <img src={user.avatar} alt='avatar'></img>
                                 <div className='card-body'>
--- src/store/reducer-slices/users/index.js
+++ src/store/reducer-slices/users/index.js
@@ -1,19 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
+import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const userSlice = createSlice({
     name: "users", // asi me refiero a esto dentro del estado
     initialState: {
-        list:[] //arranca vacio la lista de usuarios
+        list: [] //arranca vacio la lista de usuarios
     },
-    reducers:{
-
-    }   
+    reducers: {
+        setUserList: (state, action) =>{ // setUserList es una action
+            state.list = action.payload;
+        }
+    },
 })
+
+export const { setUserList} = userSlice.actions;
 
 export default userSlice.reducer; // exportamos solo el reducer para llevarlo al store que tiene el estado global con todos los reducers
 
-export const fetchAllUsers = () => () => { // funcion que retorna otra funcion
-    axios.get("https://reqres.in/api/users?per_page=12").then((response) => {}).catch((error) => console.log(error))
+export const fetchAllUsers = () => (dispatch) => { // funcion que retorna otra funcion // los dispatch me activan las actions
+    return axios.get("https://reqres.in/api/users?per_page=12").then((response) => { 
+        dispatch( setUserList(response.data.data))
+    }).catch((error) => console.log(error))
 
 }
