842c0ac360052e2b4084daf63dec5ffabb7f67cc
Puchoanimal <santiagovera280@gmail.com>
2023-04-20 21:35:03 -0300

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => (
+    <nav className='navbar navbar-dark bg-dark'>
+        <div className='container'>
+            <a className='navbar-brand' href="/">React redux app</a>
+        </div>
+    </nav>
+
+);
+
+export default Navbar;
